53871bdaf39d346e7c250e8be86085955f8de713
薛丞宏 <ihcaoe@gmail.com>
2015-06-07 14:36:35 +0800

    用舊的moses就會使訓練成功

--- 臺灣言語服務/摩西服務.py
+++ 臺灣言語服務/摩西服務.py
@@ -15,26 +15,10 @@ from 臺灣言語工具.解析整理.物件譀鏡 import 物件譀鏡
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from time import sleep
 
-class 摩西翻譯():
-	def __init__(self):
-		self.這馬目錄 = os.path.dirname(os.path.abspath(__file__))
-		self.moses模型資料夾路徑 = os.path.join(self.這馬目錄, '模型語料','翻譯模型')
-	def 試翻(self):
-		翻譯編碼器 = 語句編碼器()  # 若用著Unicdoe擴充就需要
-# 		服務 = 摩西服務端(self.moses模型資料夾路徑, 埠=8500)
-# 		服務.走()
-		
-		分析器 = 拆文分析器()
-		華語章物件 = 分析器.轉做句物件('屏東 潮州 有人 把水果紙箱  套在 身上 。')
-		
-		斷詞用戶 = 斷詞用戶端()
-		華語斷詞章物件 = 斷詞用戶.斷詞(華語章物件)
-		
-		摩西用戶 = 摩西用戶端(埠=8500, 編碼器=翻譯編碼器)
-		閩南語章物件, 翻譯結構華語章物件, 分數 = 摩西用戶.翻譯(華語斷詞章物件)
-		
-		譀鏡 = 物件譀鏡()
-		print(譀鏡.看型(閩南語章物件))
-		
 if __name__ == '__main__':
-	摩西翻譯().試翻()
+	這馬目錄 = os.path.dirname(os.path.abspath(__file__))
+	moses模型資料夾路徑 = os.path.join(這馬目錄,'模型訓練', '翻譯模型')
+	服務 = 摩西服務端(moses模型資料夾路徑, 埠=8500)
+	服務.走()
+	while True:
+		pass
