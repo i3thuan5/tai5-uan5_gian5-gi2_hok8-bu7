8c859ecc61c7420c149ece7e83d87b4767731ed6
薛丞宏 <ihcaoe@gmail.com>
2015-08-17 15:11:46 +0800

    訓練到使用的流程有通矣，閣愛整理做全自動

--- 臺灣言語服務/摩西服務.py
+++ 臺灣言語服務/摩西服務.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 import os
+'''
+from 臺灣言語服務.摩西服務 import 摩西服務
+摩西服務()
+'''
 
 from 臺灣言語工具.翻譯.摩西工具.摩西服務端 import 摩西服務端
 
-if __name__ == '__main__':
-	這馬目錄 = os.path.dirname(os.path.abspath(__file__))
-	moses模型資料夾路徑 = os.path.join(這馬目錄, '模型訓練', '翻譯模型')
-	服務 = 摩西服務端(moses模型資料夾路徑, 埠=8500)
-	服務.走()
-	服務.等()
+def 摩西服務():
+    moses模型資料夾路徑 = os.path.join(settings.BASE_DIR, '語料', '翻譯模型','閩南語')
+    服務 = 摩西服務端(moses模型資料夾路徑, 埠=8500)
+    服務.走()
+    服務.等()
