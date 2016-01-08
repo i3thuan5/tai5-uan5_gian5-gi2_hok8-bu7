2bd413aa879194ef4dca82a69b41a1bac61cbf45
薛丞宏 <ihcaoe@gmail.com>
2016-01-08 15:11:22 +0800

    服務對參數單元試驗

--- 臺灣言語服務/網址.py
+++ 臺灣言語服務/網址.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 from 臺灣言語服務.服務 import 服務
-
-
-__服務 = 服務()
+from 臺灣言語服務.模型載入 import 全部翻譯母語模型
+from 臺灣言語服務.模型載入 import 全部合成母語模型
+_服務 = 服務(
+    全部翻譯母語模型=全部翻譯母語模型,
+    全部合成母語模型=全部合成母語模型,
+)
 urlpatterns = patterns(
     '',
-    url(r'^正規化翻譯$', __服務.正規化翻譯),
-    url(r'^語音合成$', __服務.語音合成),
+    url(r'^正規化翻譯$', _服務.正規化翻譯),
+    url(r'^語音合成$', _服務.語音合成),
 )
---
+++ 臺灣言語服務/試驗/test服務單元試驗.py
@@ -0,0 +1,37 @@
+from unittest.mock import patch
+
+from django.test.client import RequestFactory
+from django.test.testcases import TestCase
+
+
+from 臺灣言語服務.服務 import 服務
+
+
+class 服務單元試驗(TestCase):
+
+    def setUp(self):
+        self.工具 = RequestFactory()
+
+    def test_會當無傳模型(self):
+        服務()
+
+    def test_會當無半個模型(self):
+        服務(全部翻譯母語模型={}, 全部合成母語模型={})
+
+    @patch('臺灣言語服務.服務.服務._正規化翻譯實作')
+    def test_正規化翻譯(self, 實作mock):
+        母語模型 = {
+            '摩西用戶端': None,
+            '辭典': None,
+            '語言模型': None,
+            '拼音': None,
+        }
+        服務功能 = 服務(全部翻譯母語模型={'母語': 母語模型}, 全部合成母語模型={})
+
+        要求 = self.工具.get('/正規化翻譯')
+        要求.POST = {
+            '查詢腔口': '母語',
+            '查詢語句': '你好'
+        }
+        服務功能.正規化翻譯(要求)
+        實作mock.assert_called_once_with(母語模型, '你好')
