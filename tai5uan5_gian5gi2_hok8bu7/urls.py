d5c19200081c20675f958c265fd306c4dd7f9150
薛丞宏 <ihcaoe@gmail.com>
2016-01-08 15:30:59 +0800

    調travis設定，而且重排版

--- tai5uan5_gian5gi2_hok8bu7/urls.py
+++ tai5uan5_gian5gi2_hok8bu7/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'tai5uan5_gian5gi2_hok8bu7.views.home', name='home'),
     url(r'^', include('臺灣言語服務.網址')),
--- 臺灣言語服務/試驗/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = '臺灣言語服務.模型載入.模型載入'
