2f8d32fc6ad7f013bc76dd516a4b4a2fb06668bc
薛丞宏 <ihcaoe@gmail.com>
2016-08-18 11:47:59 +0800

    加訓練HTK的指令，而且補join，無攏生做['sil', te , it , phinn , sil , gu , hi , sil ]

---
+++ 臺灣言語服務/management/commands/訓練HTK.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from sys import stderr
+from 臺灣言語服務.HTK模型訓練 import HTK模型訓練
+
+
+class Command(BaseCommand):
+    help = '訓練一个語言的HTK模型'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '語言',
+            type=str,
+            help='愛訓練的語言'
+        )
+
+    def handle(self, *args, **參數):
+        try:
+            語言 = 參數['語言']
+            HTK模型訓練.輸出一種語言語料('語料資料夾', 語言)
+            HTK模型訓練.訓練一个辨識模型('語料資料夾', '模型資料夾', 語言)
+        except FileNotFoundError:
+            print('資料庫無「{}」的語料！！'.format(參數['語言']), file=stderr)
