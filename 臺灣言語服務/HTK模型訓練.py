aeaf0984db71aab2c92028db8a3ed4d34f5c5d75
薛丞宏 <ihcaoe@gmail.com>
2016-08-18 08:08:16 +0800

    烏白寫輸出語料

--- 臺灣言語服務/HTK模型訓練.py
+++ 臺灣言語服務/HTK模型訓練.py
@@ -24,6 +24,9 @@ from 臺灣言語服務.語言判斷 import 語言判斷
 from 臺灣言語工具.語言模型.實際語言模型 import 實際語言模型
 from 臺灣言語工具.語音辨識.HTK工具.HTK辨識模型訓練 import HTK辨識模型訓練
 from 臺灣言語工具.語音辨識.文本音值對照表.閩南語文本音值表 import 閩南語文本音值表
+from 臺灣言語資料庫.資料模型 import 影音表
+from 臺灣言語工具.語音辨識.漢語轉辨識標仔 import 漢語轉辨識標仔
+from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 '''
 python manage.py 訓練一个語言 閩南語
 python manage.py 訓練全部語言
@@ -47,14 +50,30 @@ class HTK模型訓練(程式腳本):
         makedirs(模型資料夾, exist_ok=True)
         for 語言 in listdir(語料資料夾):
             try:
-                cls.訓練一个摩西翻譯模型(語料資料夾, 模型資料夾, 語言)
+                cls.訓練一个辨識模型(語料資料夾, 模型資料夾, 語言)
             except:
-                print('{}的摩西模型訓練失敗！！'.format(語言), file=stderr)
+                print('{}的HTK辨識模型訓練失敗！！'.format(語言), file=stderr)
                 traceback.print_exc()
                 print(file=stderr)
 
     @classmethod
-    def 訓練一个辨識模型(cls, 語料資料夾, 模型資料夾, 語言):
+    def _輸出一種語言語料(cls, 語料資料夾, 語言):
+        辨識語料資料夾 = join(語料資料夾, 語言)
+        音檔 = join(辨識語料資料夾, '音檔')
+        標仔 = join(辨識語料資料夾, '標仔')
+        for 第幾个, 影音 in enumerate(影音表.objects.filter(影音文本__isnull=False)):
+            文本 = cls._揣上尾的文本(影音.來源文本.文本)
+            標 = 漢語轉辨識標仔.物件轉音節標仔(
+                拆文分析器.分詞句物件(文本.文本佮音標格式化資料()),
+                臺灣閩南語羅馬字拼音
+            )
+            with open(join(標仔, 'im{}.lab'.format(第幾个)), 'w') as 目標txt檔案:
+                print(標, file=目標txt檔案)
+            with open(join(音檔, 'im{}.wav'.format(第幾个)), 'wb') as 目標wav檔案:
+                目標wav檔案.write(影音.音檔)
+
+    @classmethod
+    def _訓練一个辨識模型(cls, 語料資料夾, 模型資料夾, 語言):
         辨識語料資料夾 = join(語料資料夾, 語言)
         辨識模型資料夾路徑 = join(模型資料夾, 語言)
         makedirs(辨識模型資料夾路徑, exist_ok=True)
