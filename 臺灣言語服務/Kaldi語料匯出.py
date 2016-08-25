fc25c5040b39ea64de68b31e5a53e2bb1824e7f0
薛丞宏 <ihcaoe@gmail.com>
2016-08-25 22:10:52 +0800

    先加處理音素的部份

--- 臺灣言語服務/Kaldi語料匯出.py
+++ 臺灣言語服務/Kaldi語料匯出.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+import json
 from os import makedirs
 from os.path import join
 
 
 from 臺灣言語資料庫.資料模型 import 影音表
-import json
+from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 
 
-class Kaldi語料匯出():
+class Kaldi語料匯出(程式腳本):
 
     @classmethod
     def 匯出一種語言語料(cls, 語言, 語料資料夾):
@@ -19,6 +22,40 @@ class Kaldi語料匯出():
                     with cls._寫檔(訓練語料資料夾, 'reco2file_and_channel') as 音檔對應頻道:
                         with cls._寫檔(訓練語料資料夾, 'utt2spk') as 語句對應語者:
                             cls._揣影音輸出(語言, 聽拍內容, 音檔目錄, 語句目錄, 音檔對應頻道, 語句對應語者)
+
+    @classmethod
+    def 做音韻資料(cls, 語言文本, 語料資料夾):
+        全部詞 = {'SIL\tSIL'}
+        全部句 = []
+        聲類 = set()
+        韻類 = {}
+        with open(語言文本, 'r') as 檔案:
+            for 一逝 in 檔案:
+                句物件 = 拆文分析器.分詞句物件(一逝.strip())
+                一句 = []
+                for 詞物件 in 句物件.網出詞物件():
+                    try:
+                        聲韻陣列 = []
+                        for 字物件 in 詞物件.轉音(臺灣閩南語羅馬字拼音, '音值').篩出字物件():
+                            聲, 韻, 調 = 字物件.音
+                            聲韻陣列.append(聲)
+                            聲韻陣列.append(韻 + 調)
+                            聲類.add(聲)
+                            try:
+                                韻類[韻].add(韻 + 調)
+                            except:
+                                韻類[韻] = {韻 + 調}
+                        分詞 = 詞物件.看分詞()
+                        一項 = '{}\t{}'.format(分詞, ' '.join(聲韻陣列))
+                        if 'iauh' in 分詞 or 'er' in 分詞 or 'ir' in 分詞:
+                            continue
+                        全部詞.add(一項)
+                        一句.append(分詞)
+    #                     print(一項)
+                    except:
+                        pass
+                全部句.append(' '.join(一句))
+        return 全部詞, 全部句
 
     @classmethod
     def _揣影音輸出(cls, 語言, 聽拍內容, 音檔目錄, 語句目錄, 音檔對應頻道, 語句對應語者):
