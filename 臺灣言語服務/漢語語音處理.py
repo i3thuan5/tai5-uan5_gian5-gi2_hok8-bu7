37a12dab0f2984e9017cfc27ee405e25f6f0cebf
薛丞宏 <ihcaoe@gmail.com>
2016-09-20 16:45:29 +0800

    重排版

--- 臺灣言語服務/漢語語音處理.py
+++ 臺灣言語服務/漢語語音處理.py
@@ -2,7 +2,8 @@ import re
 
 
 class 漢語語音處理:
-    _切漢語韻=re.compile('.ⁿ|m̩|ŋ̩|[^ⁿ]')
+    _切漢語韻 = re.compile('.ⁿ|m̩|ŋ̩|[^ⁿ]')
+
     @classmethod
-    def 切漢語韻(cls,韻):
+    def 切漢語韻(cls, 韻):
         return cls._切漢語韻.findall(韻)
