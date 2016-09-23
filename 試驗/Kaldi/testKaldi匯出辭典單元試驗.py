164dbfc2ec2fc657118275c89008a572dd041aed
薛丞宏 <ihcaoe@gmail.com>
2016-09-23 22:11:32 +0800

    補試驗

--- 試驗/Kaldi/testKaldi匯出辭典單元試驗.py
+++ 試驗/Kaldi/testKaldi匯出辭典單元試驗.py
@@ -34,3 +34,23 @@ class Kaldi匯出辭典單元試驗(TestCase):
         self.assertEqual(len(韻類), 0)
         self.assertEqual(len(調類), 0)
         self.assertEqual(len(全部詞), 3)
+
+    def test_單引號(self):
+        分詞 = "'｜'"
+        with open(self.檔案路徑, 'w') as 檔案:
+            print(分詞, file=檔案)
+        聲類, 韻類, 調類, 全部詞 = Kaldi語料匯出._辭典資料(self.檔案路徑)
+        self.assertEqual(len(聲類), 0)
+        self.assertEqual(len(韻類), 0)
+        self.assertEqual(len(調類), 0)
+        self.assertIn("'｜'\tSIL", 全部詞)
+
+    def test_大分號(self):
+        分詞 = "﹔｜﹔"
+        with open(self.檔案路徑, 'w') as 檔案:
+            print(分詞, file=檔案)
+        聲類, 韻類, 調類, 全部詞 = Kaldi語料匯出._辭典資料(self.檔案路徑)
+        self.assertEqual(len(聲類), 0)
+        self.assertEqual(len(韻類), 0)
+        self.assertEqual(len(調類), 0)
+        self.assertIn("﹔｜﹔\tSIL", 全部詞)
