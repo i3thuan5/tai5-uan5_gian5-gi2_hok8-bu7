b97043ba8495c3c03193e2d540fadbe616ace79a
薛丞宏 <ihcaoe@gmail.com>
2016-09-30 23:10:25 +0800

    共語言相關的參數拆去設定，莫佇程式內底判斷

--- 臺灣言語服務/management/commands/訓練HTS.py
+++ 臺灣言語服務/management/commands/訓練HTS.py
@@ -1,14 +1,15 @@
+from os import makedirs
 from os.path import join, dirname
 from shutil import copyfile
 from sys import stderr
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 
 from 臺灣言語服務.HTS模型訓練 import HTS模型訓練
 from 臺灣言語服務.資料模型路徑 import 資料路徑
 from 臺灣言語服務.資料模型路徑 import 合成模型路徑
-from os import makedirs
 from 臺灣言語工具.語音合成.HTS工具.安裝HTS語音辨識程式 import 安裝HTS語音辨識程式
 
 
@@ -34,12 +35,21 @@ class Command(BaseCommand):
         安裝HTS語音辨識程式.掠htsDemoScript()
         try:
             語言 = 參數['語言']
+            服務設定 = settings.HOK8_BU7_SIAT4_TING7[語言]
             語者 = 參數['語者']
             模型資料夾 = join(資料路徑, 語言, '{}-HTS模型'.format(語者))
             語料資料夾 = join(模型資料夾, '語料')
-            HTS模型訓練.輸出一種語言語料(語料資料夾, 語言, 語者)
-            對齊聲韻結果資料夾 = HTS模型訓練.對齊聲韻(語料資料夾, 模型資料夾)
-            模型路徑 = HTS模型訓練.輸出HTS標仔問題音檔而且訓練(語料資料夾, 對齊聲韻結果資料夾, 模型資料夾)
+            HTS模型訓練.輸出一種語言語料(
+                語料資料夾, 語言, 語者,
+                服務設定['音標系統']
+            )
+            對齊聲韻結果資料夾 = HTS模型訓練.對齊聲韻(
+                語料資料夾, 模型資料夾, 服務設定['文本音值表']
+            )
+            模型路徑 = HTS模型訓練.輸出HTS標仔問題音檔而且訓練(
+                語料資料夾, 對齊聲韻結果資料夾, 模型資料夾,
+                服務設定['決策樹仔']
+            )
             上尾模型路徑 = 合成模型路徑(語言)
             makedirs(dirname(上尾模型路徑), exist_ok=True)
             copyfile(模型路徑, 上尾模型路徑)
