fe9ed9cfa29aadfba877ab6b0b8ef60d250ea5b3
薛丞宏 <ihcaoe@gmail.com>
2016-10-14 21:18:19 +0800

    加文本直接合成的介面

--- 臺灣言語服務/HTS介面.py
+++ 臺灣言語服務/HTS介面.py
@@ -33,6 +33,25 @@ class HTS介面:
         wav格式資料 = self.服務.語音合成實作(查詢腔口, 查詢語句)
         return self.音檔包做回應(b64decode(wav格式資料['data']))
 
+    @csrf_exempt
+    def 文本直接合成(self, request):
+        if request.method == 'GET':
+            連線參數 = request.GET
+        else:
+            連線參數 = request.POST
+        try:
+            查詢語句 = 連線參數['查詢語句']
+        except:
+            查詢語句 = '你｜li2 好-無｜ho2-0bo5 ？｜? 我｜gua2 足｜tsiok4 好｜ho2 ！｜!'
+        try:
+            查詢腔口 = 連線參數['查詢腔口']
+            if 查詢腔口 not in self.服務.支援腔口():
+                raise RuntimeError()
+        except:
+            查詢腔口 = '閩南語'
+        wav格式資料 = self.服務.文本直接合成(查詢腔口, 查詢語句)
+        return self.音檔包做回應(b64decode(wav格式資料['data']))
+
     def 音檔包做回應(self, 音檔):
         回應 = HttpResponse()
         回應.write(音檔)
