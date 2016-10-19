6b20cf03330ad4f5245593f1899b346df6eb8167
薛丞宏 <ihcaoe@gmail.com>
2016-10-19 12:31:55 +0800

    加試驗專用的參數

--- 臺灣言語服務/management/commands/匯出Kaldi格式資料.py
+++ 臺灣言語服務/management/commands/匯出Kaldi格式資料.py
@@ -34,6 +34,13 @@ class Command(BaseCommand):
             default='data',
             help='s5底下的資料夾名',
         )
+        parser.add_argument(
+            '--輸出試驗音檔',
+            dest='輸出試驗音檔',
+            default=False,
+            action='store_const',
+            const=True,
+        )
 
     def handle(self, *args, **參數):
         辭典資料 = Kaldi語料匯出.初使化辭典資料()
@@ -41,12 +48,14 @@ class Command(BaseCommand):
             try:
                 服務設定 = settings.HOK8_BU7_SIAT4_TING7[語言]
                 Kaldi語料匯出.匯出一種語言語料(
-                    語言, 服務設定['音標系統'], 參數['匯出路徑'], 參數['資料夾名'], 辭典資料
+                    語言, 服務設定['音標系統'],參數['輸出試驗音檔'],
+                     參數['匯出路徑'], 參數['資料夾名'], 辭典資料
                 )
             except FileNotFoundError:
                 print('資料庫無「{}」的語料！！'.format(語言), file=stderr)
-        if 參數['語言文本'] is not None:
-            服務設定 = settings.HOK8_BU7_SIAT4_TING7[參數['語言'][0]]
-            Kaldi語料匯出.辭典資料載入語句文本(參數['語言文本'], 服務設定['音標系統'], 辭典資料)
-            Kaldi語料匯出.匯出語言模型(參數['語言文本'], 參數['匯出路徑'], 參數['資料夾名'])
-        Kaldi語料匯出.匯出辭典資料(辭典資料, 參數['匯出路徑'], 參數['資料夾名'])
+        if not 參數['輸出試驗音檔']:
+            if 參數['語言文本'] is not None:
+                服務設定 = settings.HOK8_BU7_SIAT4_TING7[參數['語言'][0]]
+                Kaldi語料匯出.辭典資料載入語句文本(參數['語言文本'], 服務設定['音標系統'], 辭典資料)
+                Kaldi語料匯出.匯出語言模型(參數['語言文本'], 參數['匯出路徑'], 參數['資料夾名'])
+            Kaldi語料匯出.匯出辭典資料(辭典資料, 參數['匯出路徑'], 參數['資料夾名'])
