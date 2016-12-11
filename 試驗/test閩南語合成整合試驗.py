44423f1bbc2eba00d57bb7522886014b52e52d6c
fafoy <ihcaoe@gmail.com>
2016-12-11 18:31:07 +0800

    認真看文件處理試驗
    https://docs.djangoproject.com/en/1.10/ref/request-response/#django.http.StreamingHttpResponse.streaming_content

--- 試驗/test閩南語合成整合試驗.py
+++ 試驗/test閩南語合成整合試驗.py
@@ -1,4 +1,5 @@
 import io
+from itertools import chain
 from time import sleep
 from unittest.mock import patch
 import wave
@@ -31,7 +32,7 @@ class 閩南語合成整合試驗(TestCase):
         }
         連線回應 = self.服務功能.語音合成(連線要求)
         self.assertEqual(連線回應.status_code, 200)
-        with io.BytesIO(連線回應.streaming_content) as 資料:
+        with io.BytesIO(bytes(chain(*連線回應.streaming_content))) as 資料:
             with wave.open(資料, 'rb') as 聲音檔:
                 self.assertGreaterEqual(聲音檔.getframerate(), 16000)
 
@@ -43,7 +44,7 @@ class 閩南語合成整合試驗(TestCase):
         }
         連線回應 = self.服務功能.語音合成(連線要求)
         self.assertEqual(連線回應.status_code, 200)
-        with io.BytesIO(連線回應.streaming_content) as 資料:
+        with io.BytesIO(bytes(chain(*連線回應.streaming_content))) as 資料:
             with wave.open(資料, 'rb') as 聲音檔:
                 self.assertGreaterEqual(聲音檔.getframerate(), 16000)
 
@@ -55,7 +56,7 @@ class 閩南語合成整合試驗(TestCase):
         }
         連線回應 = self.服務功能.語音合成(連線要求)
         self.assertEqual(連線回應.status_code, 200)
-        with io.BytesIO(連線回應.streaming_content) as 資料:
+        with io.BytesIO(bytes(chain(*連線回應.streaming_content))) as 資料:
             with wave.open(資料, 'rb') as 聲音檔:
                 self.assertGreaterEqual(聲音檔.getframerate(), 16000)
                 self.assertGreater(聲音檔.getnframes(), 0)
@@ -74,7 +75,10 @@ class 閩南語合成整合試驗(TestCase):
         }
         連線回應 = self.服務功能.語音合成(連線要求)
         self.assertEqual(連線回應.status_code, 200)
-        self.assertEqual(連線回應.streaming_content, 原本連線回應.streaming_content)
+        self.assertEqual(
+            bytes(chain(*連線回應.streaming_content)),
+            bytes(chain(*原本連線回應.streaming_content))
+        )
 
     def test_無參數預設閩南語(self):
         連線要求 = RequestFactory().get('/語音合成')
@@ -88,4 +92,7 @@ class 閩南語合成整合試驗(TestCase):
         連線要求.GET = {}
         連線回應 = self.服務功能.語音合成(連線要求)
         self.assertEqual(連線回應.status_code, 200)
-        self.assertEqual(連線回應.streaming_content, 原本連線回應.streaming_content)
+        self.assertEqual(
+            bytes(chain(*連線回應.streaming_content)),
+            bytes(chain(*原本連線回應.streaming_content))
+        )
