c17f373ce6f859ffda9e8d158bec3f9197aaa281
薛丞宏 <ihcaoe@gmail.com>
2017-02-16 10:46:18 +0800

    加model記錄辨識結果

--- 臺灣言語服務/Kaldi介面.py
+++ 臺灣言語服務/Kaldi介面.py
@@ -1,12 +1,15 @@
 from base64 import b64decode
 import json
 
+from django.http.response import HttpResponse, HttpResponseServerError
+
 
 from 臺灣言語服務.Kaldi語料辨識 import Kaldi語料辨識
 from 臺灣言語工具.語音辨識.聲音檔 import 聲音檔
+from 臺灣言語服務.models import Kaldi辨識結果
 
 
-def post(self, request):
+def Kaldi辨識(self, request):
     try:
         啥人唸的 = request.POST['啥人唸的'].strip()
     except:
@@ -17,6 +20,12 @@ def post(self, request):
     ))
 
     影音 = Kaldi語料辨識.匯入音檔(語言, 啥人唸的, 聲音檔.對資料轉(資料陣列))
-    章物件 = Kaldi語料辨識.辨識音檔(影音)
-    print(章物件)
-    return
+    try:
+        章物件 = Kaldi語料辨識.辨識音檔(影音)
+        影音.Kaldi辨識結果 = Kaldi辨識結果(辨識出問題=False, 辨識結果分詞=章物件.看分詞())
+        影音.save()
+        return HttpResponse()
+    except:
+        影音.Kaldi辨識結果 = Kaldi辨識結果(辨識出問題=True, 辨識結果分詞='')
+        影音.save()
+        return HttpResponseServerError()
