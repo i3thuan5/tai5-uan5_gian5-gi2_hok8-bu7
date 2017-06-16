e212e5a514fe88c68cf8b4d7ef11b1836d4ec351
薛丞宏 <ihcaoe@gmail.com>
2017-06-16 17:01:01 +0800

    先加試驗

---
+++ 試驗/Kaldi/test匯出漢語音節fst單元試驗.py
@@ -0,0 +1,84 @@
+from os.path import join
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
+
+from django.test.testcases import TestCase
+
+
+# from 臺灣言語服務.Kaldi語料處理 import Kaldi語料處理
+from django.core.management import call_command
+
+
+class 匯出漢語音節fst單元試驗(TestCase):
+
+    def test_函式(self):
+        音節 = Kaldi語料處理.揣出漢語音節種類(
+            ['敢-若｜kan2-na2 散-步｜san3-poo7 咧｜leh4']
+        )
+        self.assertEqual(
+            音節,
+            {'kan2', 'na2', 'san3', 'poo7', 'leh4'}
+        )
+
+    def test_轉fst(self, 揣出漢語音節種類mock):
+        fst = Kaldi語料處理.轉fst格式(
+            {'kan2', 'na2', 'san3', 'poo7', 'leh4'}
+        )
+        self.assertEqual(
+            fst,
+            [
+                '0 0 leh4｜leh4 0',
+                '0 0 kan2｜kan2 0',
+                '0 0 na2｜na2 0',
+                '0 0 poo7｜poo7 0',
+                '0 0 san3｜san3 0',
+                '0 1',
+            ]
+        )
+
+    @patch('臺灣言語服務.Kaldi語料處理.Kaldi語料處理.揣出漢語音節種類')
+    def test_指令有讀(self, 揣出漢語音節種類mock):
+        揣出漢語音節種類mock.return_value = {'kan2', 'na2', 'san3', 'poo7', 'leh4'}
+        with TemporaryDirectory() as 資料夾路徑:
+            語言文本 = join(資料夾路徑, '語言文本.txt')
+            with open(語言文本, 'w') as 檔案:
+                print('敢-若｜kan2-na2 散-步｜san3-poo7 咧｜leh4', file=檔案)
+
+            call_command('Kaldi音節fst', 語言文本)
+
+            揣出漢語音節種類mock.assert_called_once_with(
+                ['敢-若｜kan2-na2 散-步｜san3-poo7 咧｜leh4']
+            )
+
+    @patch('臺灣言語服務.Kaldi語料處理.Kaldi語料處理.轉fst格式')
+    def test_指令(self, 轉fst格式mock):
+        轉fst格式mock.return_value = [
+            '0 0 leh4｜leh4 0',
+            '0 0 kan2｜kan2 0',
+            '0 0 na2｜na2 0',
+            '0 0 poo7｜poo7 0',
+            '0 0 san3｜san3 0',
+            '0 1',
+        ]
+        with TemporaryDirectory() as 資料夾路徑:
+            語言文本 = join(資料夾路徑, '語言文本.txt')
+            with open(語言文本, 'w') as 檔案:
+                print('敢-若｜kan2-na2 散-步｜san3-poo7 咧｜leh4', file=檔案)
+
+            call_command('Kaldi音節fst', 語言文本)
+
+            self.比較檔案(
+                join(資料夾路徑, '語料資料夾', 'local', 'fst', 'free-syllable.fst'),
+                [
+                    '0 0 leh4｜leh4 0',
+                    '0 0 kan2｜kan2 0',
+                    '0 0 na2｜na2 0',
+                    '0 0 poo7｜poo7 0',
+                    '0 0 san3｜san3 0',
+                    '0 1',
+                ]
+            )
+
+    def 比較檔案(self, 檔名, 資料):
+        with open(檔名) as 檔案:
+            self.assertEqual(檔案.read(), '\n'.join(資料) + '\n')
