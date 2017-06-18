1c6507c556d78ec8a4576674f0c429b2fbba543a
薛丞宏 <ihcaoe@gmail.com>
2017-06-18 13:31:31 +0800

    重排版

--- 試驗/Kaldi/test匯出漢語音節fst單元試驗.py
+++ 試驗/Kaldi/test匯出漢語音節fst單元試驗.py
@@ -25,7 +25,10 @@ class 匯出漢語音節fst單元試驗(TestCase):
     def test_揣出有音檔名嘛無要緊(self):
         音節 = Kaldi語料處理.揣出漢語音節種類(
             臺灣閩南語羅馬字拼音,
-            ['tong0000000-0000000無註明-ku0000000 敢-若｜kan2-na2 散-步｜san3-poo7 咧｜leh4']
+            [
+                'tong0000000-0000000無註明-ku0000000 '
+                '敢-若｜kan2-na2 散-步｜san3-poo7 咧｜leh4'
+            ]
         )
         self.assertEqual(
             音節,
