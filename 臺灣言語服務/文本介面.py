062f1ee5b5f2f9eff0075c5f0282f9767ef57fb4
薛丞宏 <ihcaoe@gmail.com>
2017-07-07 10:19:05 +0800

    加傳原始的漢字佮臺羅

--- 臺灣言語服務/文本介面.py
+++ 臺灣言語服務/文本介面.py
@@ -5,7 +5,7 @@ from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from 臺灣言語工具.解析整理.文章粗胚 import 文章粗胚
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-
+from 臺灣言語工具.解析整理.羅馬音仕上げ import 羅馬音仕上げ
 
 
 class 文本介面:
@@ -18,7 +18,7 @@ class 文本介面:
         else:
             連線參數 = request.POST
         try:
-            腔口參數=settings.HOK8_BU7_SIAT4_TING7[ 連線參數['查詢腔口']]
+            腔口參數 = settings.HOK8_BU7_SIAT4_TING7[連線參數['查詢腔口']]
             漢字 = 連線參數['漢字']
             音標 = 連線參數['音標']
         except:
@@ -43,8 +43,14 @@ class 文本介面:
         except:
             return {'失敗': '對齊失敗'}
         對齊結果 = {
-            '分詞': 對齊物件.轉音(腔口參數['解析拼音']).看分詞(),
+            '分詞': 對齊物件.轉音(腔口參數['音標系統']).看分詞()
         }
+        try:
+            原音物件 = 對齊物件.轉音(腔口參數['音標系統'], 函式='轉閏號調')
+            對齊結果['漢字'] = 羅馬音仕上げ.輕聲佮外來語(原音物件.看型(物件分詞符號= ' '))
+            對齊結果['音標'] = 羅馬音仕上げ.輕聲佮外來語(原音物件.看音())
+        except:
+            pass
         try:
             對齊結果['綜合標音'] = 對齊物件.綜合標音(腔口參數['字綜合標音'])
         except:
--- 試驗/文本/test閩南語對齊整合試驗.py
+++ 試驗/文本/test閩南語對齊整合試驗.py
@@ -3,48 +3,6 @@ from django.test.testcases import TestCase
 
 class 閩南語對齊整合試驗(TestCase):
 
-    def test_一般(self):
-        連線要求 = self.client.get(
-            '/漢字音標對齊',
-            {
-                '查詢腔口': '閩南語',
-                '漢字': '我愛佇公媽面頭前跪甲食暗才會使起來。',
-                '音標': (
-                    'guá ài tī kong-má bīn-thâu-tsîng ' +
-                    'kuī kah tsia̍h-àm tsiah ē-sái khí--lâi.'
-                ),
-            }
-        )
-        回應物件 = 連線要求.json()
-        self.assertIn('分詞', 回應物件)
-        self.assertIn('綜合標音', 回應物件)
-
-    def test_漢語濫英語(self):
-        連線要求 = self.client.get(
-            '/漢字音標對齊',
-            {
-                '查詢腔口': '閩南語',
-                '漢字': '伊干焦知影印尼來的外勞『Wati』爾爾。',
-                '音標': 'I kan-na tsai-iánn Ìn-nî lâi ê guā-lô ‘Wati’ niā-niā.',
-            }
-        )
-        回應物件 = 連線要求.json()
-        self.assertIn('分詞', 回應物件)
-        self.assertIn('綜合標音', 回應物件)
-
-    def test_臺語濫華語(self):
-        連線要求 = self.client.get(
-            '/漢字音標對齊',
-            {
-                '查詢腔口': '閩南語',
-                '漢字': '是華語叫做『陀螺』的「干樂」。',
-                '音標': 'sī huâ-gí kiò-tsò “陀螺” ê “kan-lo̍k”.',
-            }
-        )
-        回應物件 = 連線要求.json()
-        self.assertIn('分詞', 回應物件)
-        self.assertIn('綜合標音', 回應物件)
-
     def test_分詞愛數字調(self):
         連線要求 = self.client.get(
             '/漢字音標對齊',
@@ -71,6 +29,45 @@ class 閩南語對齊整合試驗(TestCase):
         )
         回應物件 = 連線要求.json()
         self.assertEqual(
-            回應物件['綜合標音'][0]['臺羅閏號調'],
+            回應物件['音標'],
             'suí--Suí Ê koo-niû'
         )
+
+    def test_漢字照詞分開(self):
+        連線要求 = self.client.get(
+            '/漢字音標對齊',
+            {
+                '查詢腔口': '閩南語',
+                '漢字': '媠媠的姑娘',
+                '音標': 'sui2--Sui2 E5 koo-niû',
+            }
+        )
+        回應物件 = 連線要求.json()
+        self.assertEqual(
+            回應物件['漢字'],
+            '媠 媠 的 姑娘'
+        )
+
+    def test_漢語濫英語(self):
+        連線要求 = self.client.get(
+            '/漢字音標對齊',
+            {
+                '查詢腔口': '閩南語',
+                '漢字': '伊干焦知影印尼來的外勞『Wati』爾爾。',
+                '音標': 'I kan-na tsai-iánn Ìn-nî lâi ê guā-lô ‘Wati’ niā-niā.',
+            }
+        )
+        回應物件 = 連線要求.json()
+        self.assertIn('分詞', 回應物件)
+
+    def test_臺語濫華語(self):
+        連線要求 = self.client.get(
+            '/漢字音標對齊',
+            {
+                '查詢腔口': '閩南語',
+                '漢字': '是華語叫做『陀螺』的「干樂」。',
+                '音標': 'sī huâ-gí kiò-tsò “陀螺” ê “kan-lo̍k”.',
+            }
+        )
+        回應物件 = 連線要求.json()
+        self.assertIn('分詞', 回應物件)
