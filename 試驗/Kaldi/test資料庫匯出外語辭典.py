c2d4f92734d804bf211340465cd2fa27d798f6b0
a8568730 <a8568730@hotmail.com>
2017-08-09 13:30:33 +0800

    修正排版

--- 試驗/Kaldi/test資料庫匯出外語辭典.py
+++ 試驗/Kaldi/test資料庫匯出外語辭典.py
@@ -7,7 +7,6 @@ from unittest.mock import patch
 from tempfile import TemporaryDirectory
 from os.path import join
 from django.core.management import call_command
-from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 
 
 #   /\      /\
@@ -37,14 +36,14 @@ class 資料庫匯出外語辭典試驗(TestCase):
                 '母親\tʔ- a1 b- ə2',
             ]
         )
-    
+
     def test不合法的音標(self):
         self._在外語表塞一個例('隨', '便', 'min2')
         fst = Kaldi語料處理.資料庫匯出外語辭典檔()
         self.assertEqual(fst, [])
 
     def test不合法的文本(self):
-        #6397,chhiah-chang,,赤&#399,;赤&#39918;;,,472,
+        # 6397,chhiah-chang,,赤&#399,;赤&#39918;;,,472,
         self._在外語表塞一個例('隨便', '赤&#39918', 'chhiah-chang')
         fst = Kaldi語料處理.資料庫匯出外語辭典檔()
         self.assertEqual(fst, [])
@@ -61,7 +60,7 @@ class 資料庫匯出外語辭典試驗(TestCase):
                 join(資料夾路徑, 'lexicon.txt'),
                 資料庫匯出外語辭典檔mock.return_value
             )
-        
+
     def _在外語表塞一個例(self, 外語資料='母親', 文本資料='阿母', 音標資料='a-bo2'):
         公家 = {
             '收錄者': 來源表.objects.get_or_create(名='系統管理員')[0].編號(),
