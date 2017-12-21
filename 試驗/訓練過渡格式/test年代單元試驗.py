0cd14a6f551ca98c7effa6c9651059ec9160a583
薛丞宏 <ihcaoe@gmail.com>
2017-12-21 21:21:04 +0800

    公開改公家 #259

--- 試驗/訓練過渡格式/test年代單元試驗.py
+++ 試驗/訓練過渡格式/test年代單元試驗.py
@@ -4,14 +4,14 @@ from 臺灣言語服務.models import 訓練過渡格式
 
 
 class 年代試驗(TestCase):
-    公開內容 = {'來源':  'Pigu', '種類':  '字詞', '文本': 'sui2'}
+    公家內容 = {'來源':  'Pigu', '種類':  '字詞', '文本': 'sui2'}
 
     def test_1945(self):
-        訓練過渡格式(年代='1945', **self.公開內容).full_clean()
+        訓練過渡格式(年代='1945', **self.公家內容).full_clean()
 
     def test_19xx(self):
         訓練過渡格式(年代='19xx', 來源='Pigu', 種類='字詞', 文本='sui2').full_clean()
 
     def test_無年代愛錯誤(self):
         with self.assertRaises(ValidationError):
-            訓練過渡格式(**self.公開內容).full_clean()
+            訓練過渡格式(**self.公家內容).full_clean()
--- 試驗/訓練過渡格式/test影音單元試驗.py
+++ 試驗/訓練過渡格式/test影音單元試驗.py
@@ -12,7 +12,7 @@ from django.conf import settings
 
 
 class 影音所在試驗(TestCase):
-    公開內容 = {'來源': 'Dr. Pigu', '種類':  '字詞', '年代':  '2017', }
+    公家內容 = {'來源': 'Dr. Pigu', '種類':  '字詞', '年代':  '2017', }
 
     @classmethod
     def setUpClass(cls):
@@ -31,42 +31,42 @@ class 影音所在試驗(TestCase):
         rmtree(cls.資料夾)
 
     def test_無影音無要緊(self):
-        一筆 = 訓練過渡格式(**self.公開內容)
+        一筆 = 訓練過渡格式(**self.公家內容)
         一筆.full_clean()
         一筆.save()
         self.assertIsNone(一筆.影音所在)
 
     def test_有影音上好有語者(self):
-        訓練過渡格式(影音所在=self.音檔所在, 影音語者='Pigu', **self.公開內容).full_clean()
+        訓練過渡格式(影音所在=self.音檔所在, 影音語者='Pigu', **self.公家內容).full_clean()
 
     def test_有影音無一定愛有語者(self):
-        訓練過渡格式(影音所在=self.音檔所在, **self.公開內容).full_clean()
+        訓練過渡格式(影音所在=self.音檔所在, **self.公家內容).full_clean()
 
     def test_有語者就一定愛有影音(self):
         with self.assertRaises(ValidationError):
-            訓練過渡格式(影音語者='Pigu', **self.公開內容).full_clean()
+            訓練過渡格式(影音語者='Pigu', **self.公家內容).full_clean()
 
     def test_影音所在毋是檔案(self):
         with self.assertRaises(ValidationError):
-            訓練過渡格式(影音所在=self.資料夾, **self.公開內容).full_clean()
+            訓練過渡格式(影音所在=self.資料夾, **self.公家內容).full_clean()
 
     def test_影音所在毋是wav(self):
         檔案所在 = join(self.資料夾, '別種檔案.sui')
         with open(檔案所在, 'wb') as 音檔:
             音檔.write(b'sui2' * 80000)
         with self.assertRaises(ValidationError):
-            訓練過渡格式(影音所在=檔案所在, **self.公開內容).full_clean()
+            訓練過渡格式(影音所在=檔案所在, **self.公家內容).full_clean()
 
     def test_愛轉做絕對路徑(self):
-        一筆 = 訓練過渡格式(影音所在=join('暫存', '音檔.wav'), **self.公開內容)
+        一筆 = 訓練過渡格式(影音所在=join('暫存', '音檔.wav'), **self.公家內容)
         一筆.full_clean()
         一筆.save()
         self.assertEqual(一筆.影音所在, self.音檔所在)
 
     def test_影音所在袂使是空字串(self):
         with self.assertRaises(ValidationError):
-            訓練過渡格式(影音所在='', **self.公開內容).full_clean()
+            訓練過渡格式(影音所在='', **self.公家內容).full_clean()
 
     def test_影音有通提出來用(self):
-        一筆 = 訓練過渡格式.objects.create(影音所在=self.音檔所在, **self.公開內容)
+        一筆 = 訓練過渡格式.objects.create(影音所在=self.音檔所在, **self.公家內容)
         self.assertEqual(一筆.聲音檔().時間長度(), 10.0)
