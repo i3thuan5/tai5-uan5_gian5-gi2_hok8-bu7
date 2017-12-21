99679ef9456184bd91f11daef7dfcf02c304f2c0
薛丞宏 <ihcaoe@gmail.com>
2017-12-21 20:57:04 +0800

    試驗改好矣

--- 試驗/Kaldi/test匯出一種語言語料單元試驗.py
+++ 試驗/Kaldi/test匯出一種語言語料單元試驗.py
@@ -1,5 +1,6 @@
 from os.path import join
-from tempfile import TemporaryDirectory
+from posix import remove
+from tempfile import TemporaryDirectory, NamedTemporaryFile
 
 from django.db.models.query_utils import Q
 from django.test.testcases import TestCase
@@ -12,12 +13,19 @@ from 臺灣言語服務.models import 訓練過渡格式
 
 
 class Kaldi匯出一種語言語料單元試驗(TestCase):
+    def setUp(self):
+        self.檔案 = NamedTemporaryFile(delete=False)
+        self.檔案.write(聲音檔.對參數轉(2, 16, 1, b'sui2khiau2' * 160).wav格式資料())
+        self.檔案.close()
+
+    def tearDown(self):
+        remove(self.檔案.name)
 
     def test_一條一句(self):
-        self.匯入音檔('媠巧',     'tsiang5 tsiang5')
+        self.匯入音檔('媠巧', 'tsiang5 tsiang5')
         with TemporaryDirectory() as 資料夾路徑:
             Kaldi語料匯出.匯出一種語言語料(
-                '台語',臺灣閩南語羅馬字拼音,
+                '台語', 臺灣閩南語羅馬字拼音,
                 資料夾路徑, '語料資料夾', Kaldi語料匯出.初使化辭典資料()
             )
             self.比較檔案(
@@ -52,7 +60,7 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
         )
         with TemporaryDirectory() as 資料夾路徑:
             Kaldi語料匯出.匯出一種語言語料(
-                '台語',臺灣閩南語羅馬字拼音,
+                '台語', 臺灣閩南語羅馬字拼音,
                 資料夾路徑, '語料資料夾', Kaldi語料匯出.初使化辭典資料()
             )
             self.比較檔案(
@@ -91,7 +99,7 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
             )
 
     def test_一條兩句(self):
-        一筆 = self.匯入音檔('媠巧', 'sui2')
+        一筆 = self.匯入音檔('媠巧', None)
         一筆.聽拍 = [
             {
                 '語者': '媠巧',
@@ -109,7 +117,7 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
         一筆.save()
         with TemporaryDirectory() as 資料夾路徑:
             Kaldi語料匯出.匯出一種語言語料(
-                '台語',臺灣閩南語羅馬字拼音,
+                '台語', 臺灣閩南語羅馬字拼音,
                 資料夾路徑, '語料資料夾', Kaldi語料匯出.初使化辭典資料()
             )
             self.比較檔案(
@@ -148,7 +156,7 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
         self.匯入音檔('媠巧', 'konn5 konn5')
         with TemporaryDirectory() as 資料夾路徑:
             Kaldi語料匯出.匯出一種語言語料(
-                '台語',臺灣閩南語羅馬字拼音,
+                '台語', 臺灣閩南語羅馬字拼音,
                 資料夾路徑, '語料資料夾', Kaldi語料匯出.初使化辭典資料()
             )
             self.比較檔案(
@@ -171,7 +179,7 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
         self.匯入音檔('無註明', 'konn5 konn5')
         with TemporaryDirectory() as 資料夾路徑:
             Kaldi語料匯出.匯出一種語言語料(
-                '台語',臺灣閩南語羅馬字拼音,
+                '台語', 臺灣閩南語羅馬字拼音,
                 資料夾路徑, '語料資料夾', Kaldi語料匯出.初使化辭典資料()
             )
             self.比較檔案(
@@ -194,7 +202,7 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
         一筆 = self.匯入音檔('媠巧', 'tsiang5 tsiang5')
         with TemporaryDirectory() as 資料夾路徑:
             Kaldi語料匯出.匯出一種語言語料(
-                '台語',臺灣閩南語羅馬字拼音,
+                '台語', 臺灣閩南語羅馬字拼音,
                 資料夾路徑, '語料資料夾', Kaldi語料匯出.初使化辭典資料(),
                 Q(pk=一筆.編號())
             )
@@ -223,7 +231,7 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
         self.匯入音檔('媠巧', '\ntsiang5\ntsiang5\n')
         with TemporaryDirectory() as 資料夾路徑:
             Kaldi語料匯出.匯出一種語言語料(
-                '台語',臺灣閩南語羅馬字拼音,
+                '台語', 臺灣閩南語羅馬字拼音,
                 資料夾路徑, '語料資料夾', Kaldi語料匯出.初使化辭典資料()
             )
             self.比較檔案(
@@ -232,18 +240,14 @@ class Kaldi匯出一種語言語料單元試驗(TestCase):
             )
 
     def 匯入音檔(self, 語者, 內容):
-        with TemporaryDirectory() as 資料夾路徑:
-            音檔所在 = join(資料夾路徑, 'siann.wav')
-            with open(音檔所在, 'wb') as 檔案:
-                檔案.write(聲音檔.對參數轉(2, 16, 1, b'sui2khiau2' * 160).wav格式資料())
-            return 訓練過渡格式.objects.create(
-                來源='媠巧',
-                年代='2017',
-                種類='語句',
-                影音所在=音檔所在,
-                影音語者=語者,
-                文本=內容,
-            )
+        return 訓練過渡格式.objects.create(
+            來源='媠巧',
+            年代='2017',
+            種類='語句',
+            影音所在=self.檔案.name,
+            影音語者=語者,
+            文本=內容,
+        )
 
     def 比較檔案(self, 檔名, 資料):
         with open(檔名) as 檔案:
