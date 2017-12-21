4c881dd58c5e617784e77770b58628d446b4bd44
薛丞宏 <ihcaoe@gmail.com>
2017-12-21 18:09:56 +0800

    資料庫匯出外語辭典 徙去過渡

--- 試驗/Kaldi/test資料庫匯出外語辭典單元試驗.py
+++ 試驗/Kaldi/test資料庫匯出外語辭典單元試驗.py
@@ -1,12 +1,12 @@
-from django.test.testcases import TestCase
-from 臺灣言語資料庫.資料模型 import 來源表
-from 臺灣言語資料庫.資料模型 import 版權表
-from 臺灣言語資料庫.資料模型 import 外語表
-from 臺灣言語服務.Kaldi語料處理 import Kaldi語料處理
-from unittest.mock import patch
-from tempfile import TemporaryDirectory
 from os.path import join
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
+
 from django.core.management import call_command
+from django.test.testcases import TestCase
+from 臺灣言語服務.Kaldi語料處理 import Kaldi語料處理
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語服務.models import 訓練過渡格式
 
 
 #   /\      /\
@@ -52,12 +52,6 @@ class 資料庫匯出外語辭典試驗(TestCase):
         fst = Kaldi語料處理.資料庫匯出外語辭典檔(set())
         self.assertEqual(fst, [])
 
-    def test不合法的文本(self):
-        # 6397,chhiah-chang,,赤&#399,;赤&#39918;;,,472,
-        self._在外語表塞一個例('隨便', '赤&#39918', 'chhiah-chang')
-        fst = Kaldi語料處理.資料庫匯出外語辭典檔(set())
-        self.assertEqual(fst, [])
-
     def test預設詞(self):
         self._在外語表塞一個例()
         fst = Kaldi語料處理.資料庫匯出外語辭典檔({'漂亮 s u i'})
@@ -91,28 +85,13 @@ class 資料庫匯出外語辭典試驗(TestCase):
             )
 
     def _在外語表塞一個例(self, 外語資料='母親', 文本資料='阿母', 音標資料='a-bo2'):
-        公家 = {
-            '收錄者': 來源表.objects.get_or_create(名='系統管理員')[0].編號(),
-            '來源': 來源表.objects.get_or_create(名='台文華文線頂辭典')[0].編號(),
-            '版權': 版權表.objects.get_or_create(版權='會使公開')[0].pk,
-            '種類': '字詞',
-            '語言腔口': '臺語',
-            '著作所在地': '臺灣',
-            '著作年': '2000',
-        }
-        外語內容 = {
-            '外語語言': '華語',
-            '外語資料': 外語資料.strip(),
-            '屬性': {'詞性': 'Na'},
-        }
-        外語內容.update(公家)
-        外語 = 外語表.加資料(外語內容)
-        文本內容 = {
-            '文本資料': 文本資料,
-            '音標資料': 音標資料,
-        }
-        文本內容.update(公家)
-        外語.翻母語(文本內容)
+        公開內容 = {'來源': '台文華文線頂辭典', '種類':  '字詞', '年代':  '2017', }
+
+        訓練過渡格式.objects.create(
+            文本=拆文分析器.對齊句物件(文本資料, 音標資料).看分詞(),
+            外語=外語資料,
+            **公開內容
+        )
 
     def 比較檔案(self, 檔名, 資料):
         with open(檔名) as 檔案:
