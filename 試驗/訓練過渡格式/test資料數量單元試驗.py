ae6492171be16b48ec40a1b0f335337473ae8744
薛丞宏 <ihcaoe@gmail.com>
2017-12-21 17:31:55 +0800

    補數量的試驗

---
+++ 試驗/訓練過渡格式/test資料數量單元試驗.py
@@ -0,0 +1,13 @@
+from django.test.testcases import TestCase
+from 臺灣言語服務.models import 訓練過渡格式
+
+
+class 文本分詞資料(TestCase):
+
+    def test_一開始無資料(self):
+        self.assertEqual(訓練過渡格式.objects.count(), 0)
+
+    def test_加一筆(self):
+        公開內容 = {'來源': 'Dr. Pigu', '種類':  '字詞', '年代':  '2017', }
+        訓練過渡格式.objects.create(文本='媠｜sui2', **公開內容)
+        self.assertEqual(訓練過渡格式.objects.count(), 1)
