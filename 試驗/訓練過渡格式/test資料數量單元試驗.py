1c29802bedee51169cfb59d7ac163353b8eb6e8a
薛丞宏 <ihcaoe@gmail.com>
2017-12-22 12:43:19 +0800

    新的試驗嘛攏改做公家

--- 試驗/訓練過渡格式/test資料數量單元試驗.py
+++ 試驗/訓練過渡格式/test資料數量單元試驗.py
@@ -8,6 +8,6 @@ class 文本分詞資料(TestCase):
         self.assertEqual(訓練過渡格式.objects.count(), 0)
 
     def test_加一筆(self):
-        公開內容 = {'來源': 'Dr. Pigu', '種類':  '字詞', '年代':  '2017', }
-        訓練過渡格式.objects.create(文本='媠｜sui2', **公開內容)
+        公家內容 = {'來源': 'Dr. Pigu', '種類':  '字詞', '年代':  '2017', }
+        訓練過渡格式.objects.create(文本='媠｜sui2', **公家內容)
         self.assertEqual(訓練過渡格式.objects.count(), 1)
