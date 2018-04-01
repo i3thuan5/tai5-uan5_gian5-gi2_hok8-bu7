8989f097c15d5ca116eefba9aacae8a367b1a03a
薛丞宏 <ihcaoe@gmail.com>
2018-04-01 22:39:51 +0800

    翻譯主機另外的ip

--- 臺灣言語服務/management/commands/載入服務模型.py
+++ 臺灣言語服務/management/commands/載入服務模型.py
@@ -11,11 +11,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **參數):
         pyro4主機 = getattr(settings, "PYRO4_TSU2_KI1", None)
-        if pyro4主機:
-            開放ip = '0.0.0.0'
-        else:
-            開放ip = None
-        daemon = Pyro4.Daemon(host=開放ip)
+        翻譯主機 = getattr(settings, "HUAN1_IK8_TSU2_KI1", None)
+        daemon = Pyro4.Daemon(host=翻譯主機)
         ns = Pyro4.locateNS(host=pyro4主機)
 
         uri = daemon.register(Moses服務())
