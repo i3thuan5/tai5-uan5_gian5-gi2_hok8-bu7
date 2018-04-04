83f259e0948a4cf3ec24a0eb00a5327b834e498e
薛丞宏 <ihcaoe@gmail.com>
2018-04-04 11:00:07 +0800

    語族暫時用袂著矣

--- 臺灣言語服務/management/commands/訓練Moses.py
+++ 臺灣言語服務/management/commands/訓練Moses.py
@@ -1,7 +1,6 @@
 from sys import stderr
 import traceback
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
 
@@ -33,10 +32,9 @@ class Command(BaseCommand):
         安裝摩西翻譯佮相關程式.安裝gizapp()
         安裝摩西翻譯佮相關程式.安裝moses(編譯CPU數=參數['核心數'])
         語言 = 參數['語言']
-        服務設定 = settings.HOK8_BU7_SIAT4_TING7[語言]
         Moses模型訓練.輸出全部語料(翻譯語料資料夾(語言))
         try:
-            Moses模型訓練.訓練翻譯做母語模型(語言, 服務設定['語族'])
+            Moses模型訓練.訓練翻譯做母語模型(語言)
         except FileNotFoundError:
             print('訓練時發生問題！！', file=stderr)
             traceback.print_exc()
