ce53085b707f838c92f9e4855419645942f38907
薛丞宏 <ihcaoe@gmail.com>
2018-04-05 22:02:51 +0800

    改訓練介面

--- 臺灣言語服務/management/commands/訓練Moses.py
+++ 臺灣言語服務/management/commands/訓練Moses.py
@@ -4,6 +4,7 @@ from django.core.management.base import BaseCommand
 from 臺灣言語服務.Moses模型訓練 import Moses模型訓練
 from 臺灣言語工具.翻譯.摩西工具.安裝摩西翻譯佮相關程式 import 安裝摩西翻譯佮相關程式
 from 臺灣言語服務.資料模型路徑 import 翻譯語料資料夾
+from 臺灣言語服務.資料模型路徑 import 翻譯做母語模型資料夾
 
 
 class Command(BaseCommand):
@@ -29,5 +30,7 @@ class Command(BaseCommand):
         安裝摩西翻譯佮相關程式.安裝gizapp()
         安裝摩西翻譯佮相關程式.安裝moses(編譯CPU數=參數['核心數'])
         語言 = 參數['語言']
-        Moses模型訓練.輸出全部語料(翻譯語料資料夾(語言))
-        Moses模型訓練.訓練翻譯做母語模型(語言)
+        語料資料夾 = 翻譯語料資料夾(語言)
+        模型資料夾 = 翻譯做母語模型資料夾(語言)
+        Moses模型訓練.輸出全部語料(語料資料夾)
+        Moses模型訓練.訓練翻譯做母語模型(語料資料夾, 模型資料夾)
