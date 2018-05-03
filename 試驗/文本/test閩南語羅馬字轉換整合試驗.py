6a726a65fd45741623417ef294332635bbecb608
a8568730 <a8568730@hotmail.com>
2018-05-03 11:49:15 +0800

    fix: 臺語羅馬字轉換試驗，+保留漢羅的連字符和輕聲符，調整舊試驗。

--- 試驗/文本/test閩南語羅馬字轉換整合試驗.py
+++ 試驗/文本/test閩南語羅馬字轉換整合試驗.py
@@ -2,7 +2,10 @@ from django.test.testcases import TestCase
 
 
 class 閩南語羅馬字轉換整合試驗(TestCase):
-    def test_臺羅轉換臺羅(self):
+    # 輸入可以是漢羅、全羅
+    # 羅馬字可以是臺羅傳統調、白話字傳統調、臺羅數字調、白話字數字調
+
+    def test_輸出轉換臺羅(self):
         連線回應 = self.client.get(
             '/羅馬字轉換',
             {
@@ -16,7 +19,7 @@ class 閩南語羅馬字轉換整合試驗(TestCase):
             'Lí tāinn-tio̍h'
         )
 
-    def test_臺羅轉換白話字(self):
+    def test_輸出轉換白話字(self):
         連線回應 = self.client.get(
             '/羅馬字轉換',
             {
@@ -35,27 +38,55 @@ class 閩南語羅馬字轉換整合試驗(TestCase):
             '/羅馬字轉換',
             {
                 '查詢腔口': '閩南語',
-                '查詢語句': '媠媠的姑娘',
+                '查詢語句': '媠媠e5姑娘',
             }
         )
         回應物件 = 連線回應.json()
         self.assertEqual(
-            回應物件['多元書寫'][0]['臺羅'],
-            '媠媠的姑娘'
+            回應物件['多元書寫'][0]['白話字'],
+            '媠媠ê姑娘'
         )
 
-    def test_保留使用者的斷詞方式(self):
+    def test_保留空白(self):
         連線回應 = self.client.get(
             '/羅馬字轉換',
             {
                 '查詢腔口': '閩南語',
-                '查詢語句': 'Lí --ah --honnh,',
+                '查詢語句': 'Li2 --ah4 --honnh4,',
             }
         )
         回應物件 = 連線回應.json()
         self.assertEqual(
             回應物件['多元書寫'][0]['白話字'],
             'Lí --ah --hoⁿh,'
+        )
+    
+    def test_保留漢羅的連字符(self):
+        連線回應 = self.client.get(
+            '/羅馬字轉換',
+            {
+                '查詢腔口': '閩南語',
+                '查詢語句': '定-tio̍h',
+            }
+        )
+        回應物件 = 連線回應.json()
+        self.assertEqual(
+            回應物件['多元書寫'][0]['白話字'],
+            '定-tio̍h'
+        )
+    
+    def test_保留漢羅的輕聲符(self):
+        連線回應 = self.client.get(
+            '/羅馬字轉換',
+            {
+                '查詢腔口': '閩南語',
+                '查詢語句': '定--tio̍h',
+            }
+        )
+        回應物件 = 連線回應.json()
+        self.assertEqual(
+            回應物件['多元書寫'][0]['白話字'],
+            '定--tio̍h'
         )
 
     def test_頭字大寫(self):
@@ -72,7 +103,25 @@ class 閩南語羅馬字轉換整合試驗(TestCase):
             'Lí'
         )
 
-    def test_多句轉換(self):
+    def test_逗號後壁小寫(self):
+        連線回應 = self.client.get(
+            '/羅馬字轉換',
+            {
+                '查詢腔口': '閩南語',
+                '查詢語句': 'Guá, guá',
+            }
+        )
+        回應物件 = 連線回應.json()
+        self.assertEqual(
+            回應物件['多元書寫'][0]['臺羅'],
+            'Guá,'
+        )
+        self.assertEqual(
+            回應物件['多元書寫'][1]['臺羅'],
+            'guá'
+        )
+
+    def test_換行斷開(self):
         連線回應 = self.client.get(
             '/羅馬字轉換',
             {
@@ -90,20 +139,3 @@ class 閩南語羅馬字轉換整合試驗(TestCase):
             '--Honnh'
         )
     
-    def test_逗號後壁小寫(self):
-        連線回應 = self.client.get(
-            '/羅馬字轉換',
-            {
-                '查詢腔口': '閩南語',
-                '查詢語句': 'Guá,guá',
-            }
-        )
-        回應物件 = 連線回應.json()
-        self.assertEqual(
-            回應物件['多元書寫'][0]['臺羅'],
-            'Guá'
-        )
-        self.assertEqual(
-            回應物件['多元書寫'][1]['臺羅'],
-            'guá'
-        )
