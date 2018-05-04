2594f4200909ec6cddebf1b2583ff94f40b01964
a8568730 <a8568730@hotmail.com>
2018-05-04 15:13:44 +0800

    feat: 判斷閩南語羅馬字大小寫試驗，數字調符不列入判斷。

--- 試驗/文本/test判斷閩南語羅馬字大小寫單元試驗.py
+++ 試驗/文本/test判斷閩南語羅馬字大小寫單元試驗.py
@@ -34,7 +34,7 @@ class 判斷閩南語羅馬字大小寫單元試驗(TestCase):
         self.羅馬字 = 'LíNG'
         self.預期結果 = '首字大寫'
 
-    # 鼻化音
+    # 鼻化音不列入計算
     def test鼻化音全小寫(self):
         self.羅馬字 = 'íⁿ'
         self.預期結果 = '全小寫'
@@ -46,3 +46,8 @@ class 判斷閩南語羅馬字大小寫單元試驗(TestCase):
     def test鼻化音首字大寫(self):
         self.羅馬字 = 'Líⁿ'
         self.預期結果 = '首字大寫'
+    
+    # 數字調符不列入計算
+    def test數字調符不列入判斷(self):
+        self.羅馬字 = 'LA5'
+        self.預期結果 = '全大寫'
