301461b29733bd227612c46a200a3fe3b5c16a69
薛丞宏 <ihcaoe@gmail.com>
2018-05-17 22:35:09 +0800

    加台語訓練

--- 試驗/翻譯/test台語正規化整合試驗.py
+++ 試驗/翻譯/test台語正規化整合試驗.py
@@ -10,6 +10,7 @@ from 臺灣言語服務.Moses載入 import Moses載入
 from 臺灣言語服務.Moses服務 import Moses服務
 from 臺灣言語服務.Moses介面 import Moses介面
 from 臺灣言語工具.翻譯.摩西工具.安裝摩西翻譯佮相關程式 import 安裝摩西翻譯佮相關程式
+from 臺灣言語服務.Moses模型訓練 import Moses模型訓練
 
 
 class 台語正規化整合試驗(TestCase):
@@ -19,6 +20,7 @@ class 台語正規化整合試驗(TestCase):
         super(cls, cls).setUpClass()
         安裝摩西翻譯佮相關程式.安裝gizapp()
         安裝摩西翻譯佮相關程式.安裝moses(編譯CPU數=4)
+        Moses模型訓練.訓練正規化模型('詔安腔', '漢語')
         cls.服務 = Moses服務({'台語': Moses載入.摩西翻譯模型('台語', 8500)})
         cls.locatePatch = patch('Pyro4.locateNS')
         cls.ProxyPatch = patch('Pyro4.Proxy')
