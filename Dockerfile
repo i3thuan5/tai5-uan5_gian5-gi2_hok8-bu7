08871d668b8f9557cf90b8eae011170276217b78
薛丞宏 <ihcaoe@gmail.com>
2018-06-08 22:18:41 +0800

    修看覓單元試驗的 rabbitmq

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN mkdir -p /usr/local/tai5-uan5_gian5-gi2_hok8-bu7
 WORKDIR /usr/local/tai5-uan5_gian5-gi2_hok8-bu7
 RUN pip3 install tox
 COPY . .
+RUN echo RABBIT_MQ_TSU2_KI1 = \'`/sbin/ip route|awk '/default/ { print $3 }'`\' >> tai5uan5_gian5gi2_hok8bu7/settings.py
 RUN LANG=zh_TW.utf8 tox --sitepackages -e ${TOX_ENV}
 
 # RUN echo TAI5TSUAN2HUA2 = \'`/sbin/ip route|awk '/default/ { print $3 }'`\' >> 設定/settings.py
--- tai5uan5_gian5gi2_hok8bu7/celery.py
+++ tai5uan5_gian5gi2_hok8bu7/celery.py
@@ -1,12 +1,23 @@
 from __future__ import absolute_import, unicode_literals
+
 import os
+
 from celery import Celery
+from django.conf import settings
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE', 'tai5uan5_gian5gi2_hok8bu7.settings')
 
-app = Celery('tai5uan5_gian5gi2_hok8bu7')
+try:
+    rabbitmq = 'amqp://guest:guest@{}:5672//'.format(
+        settings.RABBIT_MQ_TSU2_KI1
+    )
+except AttributeError:
+    rabbitmq = None
+
+app = Celery('tai5uan5_gian5gi2_hok8bu7', broker=rabbitmq)
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
