76d85d555686eec3d02ca18c315321c9317df2c4
fafoy <ihcaoe@gmail.com>
2018-06-18 12:39:51 +0800

    tso tsit-puann

--- 臺灣言語服務/網址.py
+++ 臺灣言語服務/網址.py
@@ -9,15 +9,21 @@ from 臺灣言語服務.Kaldi介面 import Kaldi辨識
 from 臺灣言語服務.Kaldi介面 import 看對齊結果
 from 臺灣言語服務.Kaldi介面 import Kaldi對齊
 from 臺灣言語服務.文本介面 import 文本介面
+from 臺灣言語服務.斷詞介面 import 斷詞介面
 
 _Moses = Moses介面()
 _HTS = HTS介面()
 
 urlpatterns = [
+    url(r'^羅馬字轉換$', 文本介面.羅馬字轉換),
+    url(r'^漢字音標對齊$', 文本介面.漢字音標對齊),
+
+    url(r'^標漢字音標$', _Moses.標漢字音標),
+    url(r'^標漢羅$', 斷詞介面.標漢羅),
+
     url(r'^正規化翻譯支援腔口$', _Moses.正規化翻譯支援腔口),
     url(r'^正規化翻譯$', _Moses.正規化翻譯),
-    url(r'^標漢字音標$', _Moses.標漢字音標),
-    url(r'^漢字音標對齊$', 文本介面.漢字音標對齊),
+
     url(r'^語音合成支援腔口$', _HTS.語音合成支援腔口),
     url(r'^語音合成$', _HTS.語音合成),
     url(r'^文本直接合成$', _HTS.文本直接合成),
@@ -29,6 +35,4 @@ urlpatterns = [
     url(r'^資料庫影音檔案/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT, 'show_indexes': False
     }),
-
-    url(r'^羅馬字轉換$', 文本介面.羅馬字轉換),
 ]
--- 試驗/斷詞/test使用者定義辭典試驗.py
+++ 試驗/斷詞/test使用者定義辭典試驗.py
@@ -1,10 +1,32 @@
 import json
+from unittest.mock import patch
 
 from django.test.testcases import TestCase
+from django.urls.base import resolve
+
+
+from 臺灣言語服務.斷詞介面 import 斷詞介面
+from 臺灣言語服務.Moses服務 import Moses服務
+from 臺灣言語工具.辭典.現掀辭典 import 現掀辭典
+from 臺灣言語工具.語言模型.實際語言模型 import 實際語言模型
+from 臺灣言語工具.音標系統.台語 import 新白話字
 
 
 class 使用者定義辭典試驗(TestCase):
-    def test_短詞翻譯(self):
+    def test_有對應函式(self):
+        對應 = resolve('/標漢羅')
+        self.assertEqual(對應.func, 斷詞介面.標漢羅)
+
+    @patch('臺灣言語服務.斷詞介面.斷詞介面.服務')
+    def test_提供辭典(self, 服務Mock):
+        服務Mock.return_value.支援腔口.return_value = ['台語', '客話', 'Kaxabu']
+        服務Mock.return_value = Moses服務({'台語': {
+            '解析拼音': 新白話字,
+            '辭典': 現掀辭典,
+            '語言模型': 實際語言模型,
+        }})
+
+#  with patch.dict(foo, {'newkey': 'newvalue'}, clear=True):
         連線回應 = self.client.get(
             '/標漢羅', {
                 '查詢腔口': '台語',
@@ -18,3 +40,6 @@ class 使用者定義辭典試驗(TestCase):
         回應物件 = 連線回應.json()
         self.assertIn('分詞', 回應物件)
         self.assertIn('多元書寫', 回應物件)
+
+    def test_對濟失敗(self):
+        self.fail()
