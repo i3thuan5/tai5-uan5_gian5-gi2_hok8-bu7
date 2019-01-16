27ddb7f26f75dd6623d05e7204a03311b9f68665
薛丞宏 <ihcaoe@gmail.com>
2019-01-16 14:30:18 +0800

    lexicon 部份外語

--- 試驗/Kaldi/test匯出Kaldi的lexicon單元試驗.py
+++ 試驗/Kaldi/test匯出Kaldi的lexicon單元試驗.py
@@ -25,6 +25,23 @@ class 匯出lexicon單元試驗(TestCase):
                 self.assertIn('e5\t', tsuan)
                 self.assertIn('sui2-koo1-niu5\ts-', tsuan)
 
+    def test_部份無羅馬字(self):
+        with TemporaryDirectory() as 資料夾路徑:
+            來源 = join(資料夾路徑, 'su.txt')
+            with open(來源, 'wt') as tong:
+                print('B-sann', file=tong)
+            with io.StringIO() as out:
+                call_command(
+                    '匯出Kaldi的lexicon', '台語', '拆做音素',
+                    來源, 資料夾路徑, stdout=out
+                )
+            結果 = join(資料夾路徑, 'lexicon.txt')
+            with open(結果) as kiatko:
+                tsuan = kiatko.read()
+                self.assertIn('B-sann\t', tsuan)
+                self.assertIn('e5\t', tsuan)
+                self.assertIn('sui2-koo1-niu5\ts-', tsuan)
+
     def test_有詞的數量(self):
         with TemporaryDirectory() as 資料夾路徑:
             來源 = join(資料夾路徑, 'su.txt')
